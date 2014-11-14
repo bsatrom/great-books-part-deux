e596040826731c1bcdaf4fae6f6ff74caa41fabd
Brandon Satrom <brandon@kendoui.com>
2014-11-14 18:20:37 +0000

    adding settings screen for service keys

---
+++ app/js/settings.js
@@ -0,0 +1,22 @@
+window.app = window.app || {};
+
+(function() {
+  var appSettings = {
+
+      everlive: {
+          apiKey: '8tvzSOnNUmFUnYnj', //KEY HERE
+          scheme: 'http'
+      },
+
+      eqatec: {
+          productKey: '', //KEY HERE
+          version: '1.0.0.0' 
+      },
+      
+      feedback: {
+          apiKey: '' //KEY HERE
+      },
+  };
+
+  window.app.settings = appSettings;
+}());
